78fa5ce37b39e8a4dfb75a611fd6c5fdb6ef8235
Manas Doshi <doshimanas02@github.com>
2025-06-04 03:17:54 +0530

    add breaking change to health endpoint

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('ABC...')
   res.send('ok')
 })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
-  throw('ABC...')
 })
